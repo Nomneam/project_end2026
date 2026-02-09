5c4d4f4b38b685d2517e992b226b4355b1d1cadb
NezBer <u650406401758@ms.kbu.ac.th>
2026-02-09 14:29:35 +0700

    ดึง navbar มาใช้

--- view/page_category.py
+++ view/page_category.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, abort, session, request, url_for
 from dotenv import load_dotenv
+from view.navbar import load_nav_categories
 import pymysql
 import os
 import json
@@ -10,4 +11,5 @@ page_cat_bp = Blueprint("page_cat",__name__)
 
 @page_cat_bp.route("/page_category")
 def page_category():
-    return render_template("page_category.html")
+    categories = load_nav_categories()
+    return render_template("page_category.html",categories=categories)
